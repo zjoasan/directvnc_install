78a4d5c9d62dc0af375268dea10e6a45872923a8
zjoasan <joakim.s@webbkontakt.net>
2016-04-01 16:32:03 +0200

    Update install_vnc.sh

--- install_vnc.sh
+++ install_vnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
 sudo apt-get --show-progress -y install dialog directvnc  2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing dialog and directvnc" --gauge "\nPlease wait...\n" 11 70
-wget -q https://github.com/zjoasan/directvnc_install/blob/master/install_vnc.zip?raw=true
+wget -O install_vnc.zip https://github.com/zjoasan/directvnc_install/blob/master/install_vnc.zip?raw=true
 unzip -q -o install_vnc.zip 
 chmod +x /home/osmc/vnc-client/vnc_ip_server.sh 2>&1
 chmod +x /home/osmc/vnc-client/vncstart2.sh 2>&1
